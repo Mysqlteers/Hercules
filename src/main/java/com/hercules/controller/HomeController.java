717045db1bd89cd7a59bcaa84c0315c493265359
emilnorsker <emilgraaenorsker@gmail.com>
2020-12-01 15:31:33 +0100

    create-case-modal.4.Commit.Hercules  // finished

--- src/main/java/com/hercules/controller/HomeController.java
+++ src/main/java/com/hercules/controller/HomeController.java
@@ -1,20 +1,20 @@
 package com.hercules.controller;
 
+import com.hercules.model.Case;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController
 {
     @GetMapping
-    public String inex() {
+    public String index(Model model) {
+
+        Case newcase = new Case();
+newcase.setDescription("im a very goodest boi");
+        newcase.setCaseId(new Long(100));
+        model.addAttribute("newCase", newcase); //needed for modal to create 'new case'
         return "index";
     }
-
-
-    @GetMapping(value="/modal")
-    public String index() {
-        return "case-modal";
-    }
-
 }
