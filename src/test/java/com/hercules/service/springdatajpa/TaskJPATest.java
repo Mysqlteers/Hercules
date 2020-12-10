6c8def70e6326a15cca47b933ff9d41410814fe7
EmilNordahn <60731699+EmilNordahn@users.noreply.github.com>
2020-12-10 09:56:41 +0100

    Rettet tests for TaskJPA, nu checker slet

--- src/test/java/com/hercules/service/springdatajpa/TaskJPATest.java
+++ src/test/java/com/hercules/service/springdatajpa/TaskJPATest.java
@@ -3,23 +3,47 @@ package com.hercules.service.springdatajpa;
 import com.hercules.model.Contact;
 import com.hercules.model.Task;
 import com.hercules.service.TaskService;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class TaskJPATest {
 
     @Autowired
     TaskService taskService;
 
+    String TASK_NAME = "Test task";
+    String TEST_DEADLINE = "Test deadline";
 
     @Test
+    @Order(1)
     void canSaveAndCanFindByTaskId() {
-        Task newTask = new Task();
+        Task newTask = new Task(TASK_NAME);
         taskService.save(newTask);
         assert(taskService.findByTaskId(newTask.getTaskId()).isPresent());
+    }
+
+    @Test
+    @Order(2)
+    void canUpdateTask() {
+        Task newTask = taskService.findByName(TASK_NAME).get();
+        newTask.setDeadline(TEST_DEADLINE);
+        newTask = taskService.save(newTask);
+
+        assert(taskService.findByTaskId(newTask.getTaskId()).get().getDeadline().equals(TEST_DEADLINE));
+    }
+
+    @Test
+    @Order(3)
+    void canDeleteTask() {
+        taskService.deleteById(taskService.findByName(TASK_NAME).get().getTaskId());
+        assert(taskService.findByName(TASK_NAME).isEmpty());
     }
 
 }
