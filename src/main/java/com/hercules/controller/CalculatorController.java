f57821324577afe6f86c3c6606a8d82d119817ae
DanielMejlvang <58211711+caerKEA@users.noreply.github.com>
2020-12-16 15:36:43 +0100

    List over alle jobtyper/materialer samt slet funktion

--- src/main/java/com/hercules/controller/CalculatorController.java
+++ src/main/java/com/hercules/controller/CalculatorController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class CalculatorController {
@@ -16,5 +17,18 @@ public class CalculatorController {
         model.addAttribute("jobTypeList", jobTypeService.findAllByMaterialOrderByDescriptionAsc(false));
         model.addAttribute("materialList", jobTypeService.findAllByMaterialOrderByDescriptionAsc(true));
         return "calculator";
+    }
+
+    @GetMapping("/jobTypeList")
+    public String jobTypeList(Model model) {
+        model.addAttribute("jobTypeList", jobTypeService.findAllByMaterialOrderByDescriptionAsc(false));
+        model.addAttribute("materialList", jobTypeService.findAllByMaterialOrderByDescriptionAsc(true));
+        return "jobTypeList";
+    }
+
+    @GetMapping("/deleteJobType/{jobTypeId}")
+    public String deleteJobType(@PathVariable("jobTypeId") Long jobTypeId) {
+        jobTypeService.deleteById(jobTypeId);
+        return "redirect:/jobTypeList";
     }
 }
